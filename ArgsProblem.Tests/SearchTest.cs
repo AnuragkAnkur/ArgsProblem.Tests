efc748c53d253cc1acb23fc42599f81e64bd4906
Anurag <anuragankur1107@gmail.com>
2019-10-19 02:06:47 +0530

    Added 3rd test to search for atleast 1 island

--- ArgsProblem.Tests/SearchTest.cs
+++ ArgsProblem.Tests/SearchTest.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -11,14 +9,14 @@ namespace ArgsProblem.Tests
 {
     public class SearchTest : IDisposable
     {
-        private ChromeDriver driver;
+        private readonly ChromeDriver _driver;
 
         public SearchTest()
         {
             var options = new ChromeOptions();
-            driver = new ChromeDriver(".", options);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(100);
-            driver
+            _driver = new ChromeDriver(".", options);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(100);
+            _driver
                 .Navigate()
                 .GoToUrl(new Uri(
                     "https://codility-frontend-prod.s3.amazonaws.com/media/task_static/qa_csharp_search/862b0faa506b8487c25a3384cfde8af4/static/attachments/reference_page.html"));
@@ -27,47 +25,59 @@ namespace ArgsProblem.Tests
         [Fact]
         public void test_If_SearchField_Exists()
         {
-            var inputTextElements = driver.FindElements(By.Id("search-input"));
+            var inputTextElements = _driver.FindElements(By.Id("search-input"));
             Assert.Equal(1, inputTextElements.Count);
 
-            var searchButtons = driver.FindElements(By.Id("search-button"));
+            var searchButtons = _driver.FindElements(By.Id("search-button"));
             Assert.Equal(1, searchButtons.Count);
         }
 
         [Fact]
         public void test_If_Empty_Text_Search_Is_Forbidden()
         {
-            // Example how to use the driver:
             TypeInSearchBar(string.Empty);
             Search();
             ExplicitWaitForResults(TimeSpan.FromMilliseconds(100));
 
-            var elements = driver.FindElements(By.Id("error-empty-query"));
+            var elements = _driver.FindElements(By.Id("error-empty-query"));
             Assert.Equal(1, elements.Count);
             var text = elements.First().Text;
             Assert.Equal("Provide some query", text);
         }
 
+        [Fact]
+        public void test_Atleast_One_Island_Is_Found_For_Isla()
+        {
+            TypeInSearchBar("isla");
+            Search();
+            ExplicitWaitForResults(TimeSpan.FromMilliseconds(100));
+
+            var unOrderLists = _driver.FindElement(By.Id("search-results"));
+            var lists = unOrderLists.FindElements(By.TagName("li"));
+            Assert.NotEmpty(lists);
+            Assert.NotEmpty(lists.First().Text);
+        }
+
         private void TypeInSearchBar(string text)
         {
-            var inputTextElement = driver.FindElement(By.Id("search-input"));
+            var inputTextElement = _driver.FindElement(By.Id("search-input"));
             inputTextElement.SendKeys(text);
         }
 
         private void Search()
         {
-            driver.FindElement(By.Id("search-button")).Click();
+            _driver.FindElement(By.Id("search-button")).Click();
         }
 
         public void ExplicitWaitForResults(TimeSpan waitingTime)
         {
-            var webDriverWait = new WebDriverWait(driver, waitingTime);
+            var webDriverWait = new WebDriverWait(_driver, waitingTime);
             webDriverWait.Until(driver => driver.FindElement(By.Id("search-results")));
         }
 
         public void Dispose()
         {
-            driver?.Dispose();
+            _driver?.Dispose();
         }
     }
 }
